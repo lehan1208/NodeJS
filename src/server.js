1fda36493afcec8430a211976c829e070f9cef3a
Han <101984174+HaanLee@users.noreply.github.com>
2022-07-07 09:15:20 +0700

    update

--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@ import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine';
 import initWebEngine from './route/web.js';
 import 'dotenv/config';
+import connectDB from './config/connectDB';
 
 let app = express();
 //config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 //  PORT === undefined => 6969
