d0dd4496acf65b795268023842c3452a4867ad92
Han <101984174+HaanLee@users.noreply.github.com>
2022-07-11 22:42:20 +0700

    Update create new user

--- src/services/userService.js
+++ src/services/userService.js
@@ -118,8 +118,9 @@ let createNewUser = (data) => {
         lastName: data.lastName,
         address: data.address,
         phoneNumber: data.phoneNumber,
-        gender: data.gender === '1' ? true : false,
-        roleId: data.role,
+        gender: data.gender,
+        roleId: data.roleId,
+        positionId: data.positionId,
       });
       resolve({
         errCode: 0,
